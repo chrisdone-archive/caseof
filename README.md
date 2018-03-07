9a7f6bb94d92c813f06732c0028808b6825b0e64
Chris Done <chrisdone@gmail.com>
2018-03-07 12:14:08 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# caseof
+
+A simple way to query constructors, like cases but slightly more
+concise.
+
+Aimed at sum types with many constructors:
+
+``` haskell
+data Wiggle = Woo Int Char | Wibble Int deriving Show
+```
+
+There is a case predicate:
+
+``` haskell
+> $(isCaseOf 'Woo) (Woo 5 'a')
+True
+```
+
+There is a `Maybe`-based matcher:
+
+``` haskell
+> $(maybeCaseOf 'Woo) (Woo 1 'a')
+Just (1,'a')
+```
+
+There is a combinator which calls your function with n arguments, or
+passes the whole value to an "else" clause.
+
+``` haskell
+> $(caseOf 'Woo) (\x y -> show x ++ show y) (const "") (Wibble 5)
+""
+```
+
+This allows them to be nested:
+
+```haskell
+> $(caseOf 'Woo) (\x y -> show x ++ show y) (const "") (Woo 5 'a')
+"5'a'"
+> $(caseOf 'Woo) (\x y -> show x ++ show y) ($(caseOf 'Wibble) show (const "")) (Woo 5 'a')
+"5'a'"
+```
+
+What's the point of `caseOf`? To more easily dispatch on functions:
+
+```haskell
+handleHuman name age = ...
+handleMachine id = ..
+handleWithDefault =
+   $(caseOf 'Human) handleHuman .
+   $(caseOf 'Machine) handleMachine
+```
+
+This applies to any kind of "case" that you'd like to refactor into a function.
---
+++ src/CaseOf.hs
@@ -0,0 +1,81 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module CaseOf where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+-- | Create a predicate that returns true if its argument is the given constructor.
+isCaseOf :: Name -> Q Exp
+isCaseOf input = do
+  name <- nameAsValue input
+  pure
+    (LamCaseE
+       [ Match (RecP name []) (NormalB (ConE 'True)) []
+       , Match WildP (NormalB (ConE 'False)) []
+       ])
+
+-- | Return Just (x, y, ..) for the constructor C x y .., or Nothing.
+maybeCaseOf :: Name -> Q Exp
+maybeCaseOf input = do
+  name <- nameAsValue input
+  info <- reify name
+  case info of
+    DataConI _ ty _ ->
+      pure
+        (LamCaseE
+           [ Match
+               (ConP name (map patI [1 .. arity ty]))
+               (NormalB (AppE (ConE 'Just) (TupE (map varI [1 .. arity ty]))))
+               []
+           , Match WildP (NormalB (ConE 'Nothing)) []
+           ])
+    _ -> fail ("Invalid data constructor " ++ pprint input)
+  where
+    varI i = VarE (mkName ("v" ++ show i))
+    patI i = VarP (mkName ("v" ++ show i))
+    arity (ForallT _ _ t) = arity t
+    arity (AppT (AppT ArrowT _) y) = 1 + arity y
+    arity _ = 0
+
+-- | Call a function with arguments from the constructor if it
+-- matches, or pass it to the wildcard function.
+caseOf :: Name -> Q Exp
+caseOf input = do
+  name <- nameAsValue input
+  info <- reify name
+  case info of
+    DataConI _ ty _ ->
+      pure
+        (LamE [VarP f, VarP nil]
+           (LamCaseE
+              [ Match
+                  (ConP name (map patI [1 .. arity ty]))
+                  (NormalB (foldl AppE (VarE f) (map varI [1 .. arity ty])))
+                  []
+              , Match (VarP this) (NormalB (AppE (VarE nil) (VarE this))) []
+              ]))
+    _ -> fail ("Invalid data constructor " ++ pprint input)
+  where
+    f = mkName "f"
+    this = mkName "this"
+    nil = mkName "nil"
+    varI i = VarE (mkName ("v" ++ show i))
+    patI i = VarP (mkName ("v" ++ show i))
+    arity (ForallT _ _ t) = arity t
+    arity (AppT (AppT ArrowT _) y) = 1 + arity y
+    arity _ = 0
+
+-- | Return the name if it is a value constructor, otherwise lookup a
+-- value name.
+nameAsValue :: Name -> Q Name
+nameAsValue name =
+  if nameSpace name == Just DataName
+    then pure name
+    else do
+      mname <- lookupValueName (nameBase name)
+      case mname of
+        Nothing -> fail ("Not in scope constructor " ++ pprint name)
+        Just n -> pure n
